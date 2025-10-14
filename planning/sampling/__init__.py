58701947db67c6949df5a5fddb496a5da93afe09
Kim-JeongHan <kimjh9813@naver.com>
2025-10-14 12:48:27 +0900

    [FIX] Add pythonpath for run tests

--- planning/sampling/__init__.py
+++ planning/sampling/__init__.py
@@ -1,11 +1,13 @@
 """Sampling-based path planning algorithms."""
 
+from ..collision import ObstacleCollisionChecker
 from .rrt import RRT, RRTConfig, RRTConnect, RRTConnectConfig
 from .sampler import GoalBiasedSampler, Sampler, UniformSampler
 
 __all__ = [
     "RRT",
     "GoalBiasedSampler",
+    "ObstacleCollisionChecker",
     "RRTConfig",
     "RRTConnect",
     "RRTConnectConfig",
